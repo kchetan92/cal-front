726861575525775456e0bceae8d4d7d27a9c0564
chetan <chetan@chetans-MacBook-Pro.local>
2018-04-02 21:52:28 -0400

    styling done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -29722,4 +29722,10 @@ __WEBPACK_IMPORTED_MODULE_1__home_deployer_sites_node_foundation_customizer_node
 	$('#calendar').clndr({
 		daysOfTheWeek: ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
 	});
+	$.get("http://api.apixu.com/v1/current.json?key=3abb9fdca0e74c8495071745180204&q=ann arbor", function(payload) {
+		console.log(payload)
+		$('#icons').css("background-image", "url(" + payload.current.condition.icon + ")")
+		$('#temp').text(payload.current.temp_f+"°F")
+		$('#condition').text(payload.current.condition.text)
+	})
 })()
--- app/scripts/src/_includes/3page.js
+++ app/scripts/src/_includes/3page.js
@@ -2,4 +2,10 @@
 	$('#calendar').clndr({
 		daysOfTheWeek: ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
 	});
+	$.get("http://api.apixu.com/v1/current.json?key=3abb9fdca0e74c8495071745180204&q=ann arbor", function(payload) {
+		console.log(payload)
+		$('#icons').css("background-image", "url(" + payload.current.condition.icon + ")")
+		$('#temp').text(payload.current.temp_f+"°F")
+		$('#condition').text(payload.current.condition.text)
+	})
 })()
