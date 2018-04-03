eca1a6652490121d0a28cd9554d0b26598257618
chetan <chetan@chetans-MacBook-Pro.local>
2018-04-03 02:22:30 -0400

    styles

--- app/scripts/src/_includes/3page.js
+++ app/scripts/src/_includes/3page.js
@@ -1,6 +1,12 @@
 (function(){
 	$('#calendar').clndr({
-		daysOfTheWeek: ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+		daysOfTheWeek: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+	});
+	$('.photo-container').slick({
+		autoplay: true,
+  		autoplaySpeed: 5000,
+  		arrows: false,
+  		fade: true
 	});
 	$.get("http://api.apixu.com/v1/current.json?key=3abb9fdca0e74c8495071745180204&q=ann arbor", function(payload) {
 		console.log(payload)
